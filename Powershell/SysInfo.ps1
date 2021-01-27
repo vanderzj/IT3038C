a25f9710fd92c9d5e958ba56bf39b5d09426ad0b
Zack Vanderpool <zvanderpool99@gmail.com>
2021-01-26 19:19:32 -0500

    sysinfo powershell script

--- Powershell/SysInfo.ps1
+++ Powershell/SysInfo.ps1
@@ -1,7 +1,12 @@
 ﻿function getIP{
     (Get-NetIPAddress).IPv4Address | Select-String "192*"
 }
-
 $IP = getIP
+$date = Get-Date -Format "dddd mm/dd/yyyy HH:mm K"
+$version = $Host.version.Major
+ 
+$Body = "This machine's IP is $IP. User is $ENV:Username. Hostname is $env:computername. PowerShell version is $version. Today's date is $date"
 
-Write-host($IP)
+Write-Host($Body)
+
+#Send-MailMessage -To "vanderzj@uc.mail.edu" -From "zvanderpool99@gmail.com" -Subject "IT3038C Windows SysInfo" -Body $Body -smtpServer smtp.google.com -port 587 -UseSSL -Credential (Get-Credential)
